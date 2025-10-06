6df169fb1e29487f86ce317f1860841eb03b9ca7
Manuel David Arias <davidgonzalez0519@gmail.com>
2025-10-05 23:07:56 -0500

    feat (logging): implement structured logging and global error management

--- src/infrastructure/controllers/checkpoints.controller.ts
+++ src/infrastructure/controllers/checkpoints.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Post, Body, ValidationPipe, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe, HttpCode, HttpStatus, Inject } from '@nestjs/common';
 import { CreateCheckpointUseCase } from '../../application/use-cases/create-checkpoint.use-case';
 import { CreateCheckpointDto } from './dtos/create-checkpoint.dto'; 
+import { LOGGER_PROVIDER_TOKEN } from '../logger/logger.constants';
+import { Logger } from 'pino';
 
 @Controller('checkpoints')
 export class CheckpointsController {
-  constructor(private readonly createCheckpointUseCase: CreateCheckpointUseCase) {}
+  constructor(
+    private readonly createCheckpointUseCase: CreateCheckpointUseCase,
+    @Inject(LOGGER_PROVIDER_TOKEN) private readonly logger: Logger,
+    ) {}
 
   @Post()
   @HttpCode(HttpStatus.ACCEPTED) // Devuelve 202 Accepted, ideal para flujos asíncronos
   async createCheckpoint(@Body(new ValidationPipe()) data: CreateCheckpointDto) {
+    this.logger.info({ data }, 'Received request to create checkpoint.');
     await this.createCheckpointUseCase.execute(data);
+    this.logger.info({ data }, 'Checkpoint processed by use case.');
     return { message: 'Checkpoint received and is being processed.' };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
-// src/main.ts
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionLoggerFilter } from './infrastructure/common/filters/global-exception-logger.filter';
+import { LOGGER_PROVIDER_TOKEN } from './infrastructure/logger/logger.constants';
+import { Logger } from 'pino';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); // Habilita la validación de DTOs
+  const app = await NestFactory.create(AppModule, {
+    // Disable Nest's default logger to use our own
+    logger: false,
+  });
+
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  const appLogger = app.get<Logger>(LOGGER_PROVIDER_TOKEN);
   
+  app.useGlobalPipes(new ValidationPipe()); // Habilita la validación de DTOs
+  app.useGlobalFilters(new GlobalExceptionLoggerFilter(httpAdapterHost, appLogger));
+  
+  const port = process.env.PORT || 3000;
   app.enableShutdownHooks();
-  await app.listen(3000);
+ 
+  await app.listen(port);
+  appLogger.info(
+    `Service started on port ${port}`,
+  );
 }
 bootstrap();
