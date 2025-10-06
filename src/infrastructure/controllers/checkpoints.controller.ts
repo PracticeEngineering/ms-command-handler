6db9185e335be988c7c20b2210bca3d104525435
Manuel David Arias <davidgonzalez0519@gmail.com>
2025-10-05 23:36:58 -0500

    HotFix: Added type to import pino library

--- src/infrastructure/controllers/checkpoints.controller.ts
+++ src/infrastructure/controllers/checkpoints.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Body, ValidationPipe, HttpCode, HttpStatus, Inject } 
 import { CreateCheckpointUseCase } from '../../application/use-cases/create-checkpoint.use-case';
 import { CreateCheckpointDto } from './dtos/create-checkpoint.dto'; 
 import { LOGGER_PROVIDER_TOKEN } from '../logger/logger.constants';
-import { Logger } from 'pino';
+import type { Logger } from 'pino';
 
 @Controller('checkpoints')
 export class CheckpointsController {
